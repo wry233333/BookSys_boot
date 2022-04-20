bb789a145dbb4d36948c372c1d75c09c2c04164b
WRY <wry233333@gmail.com>
2022-04-20 16:17:34 +0800

    Initial commit

---
+++ src/main/java/com/wry333/booksys_boot/BookSysBootApplication.java
@@ -0,0 +1,13 @@
+package com.wry333.booksys_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSysBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookSysBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wry333/booksys_boot/BookSysBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wry333.booksys_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSysBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
